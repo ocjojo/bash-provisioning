320bc111f2e7f3e46052b3b688a9ed8f6428dc7f
Lukas Ehnle <l.ehnle@web.de>
2019-01-03 03:08:28 +0100

    install dependencies within docker install

--- centos/install_docker.sh
+++ centos/install_docker.sh
@@ -4,7 +4,7 @@ if ! function_exists install_docker 2> /dev/null; then
 install_docker() {
   if is_not_installed "docker"; then
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    yum install docker-ce
+    yum install yum-utils device-mapper-persistent-data lvm2docker-ce
     kick enable docker
     kick start docker
   fi
